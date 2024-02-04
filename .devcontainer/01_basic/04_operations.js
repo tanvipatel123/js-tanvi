0c62167ea495e2587937f6139a3fe8231613b236
Tanvi sonani <109598478+tanvipatel123@users.noreply.github.com>
2024-02-04 16:19:29 +0000

    Operations

---
+++ .devcontainer/01_basic/04_operations.js
@@ -0,0 +1,36 @@
+let value = 3
+let negvalue = -value
+console.log(negvalue);//converting in negative value
+
+//basic operations....
+console.log(2+2);
+console.log(2-2);
+console.log(2*2);
+console.log(2**2);//2 ka power ata hai(2**3)
+console.log(2/3);
+console.log(2%3);
+
+let str1 = "Tanvi"
+let str2 =" sonani"
+console.log(str1+str2);
+
+console.log("1" + 2);//12
+console.log("1" + 2 + 2);//122
+console.log(1 + 2 + "2");//32
+
+console.log(true);//true
+console.log(+ true);//1
+//console.log(true + );//error
+console.log(+""); // 0
+
+
+//assignment operator ...
+let num1, num2,num3
+num1 = num2 = num3 = 2 +2
+
+let gameCounter = 100
+gameCounter++;
+//++gameCounter ; 101
+console.log(gameCounter);//increment hoga 101 ayega
+
+
