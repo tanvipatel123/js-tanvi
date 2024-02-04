3f53ba788df8f13048f0d46f6ea31594e60879a7
Tanvi sonani <109598478+tanvipatel123@users.noreply.github.com>
2024-02-04 12:55:13 +0000

    First commit

---
+++ .devcontainer/01_basic/01_variables.js
@@ -0,0 +1,19 @@
+const accountId = 14484
+let accountEmail = "tanu@gmail.com"
+var accountPassword = "12345"
+accountCity ="Ahmedabad"
+
+//accountId = 2// not allowed
+
+accountEmail = "tanvi@123"
+accountPassword = "4567"
+accountCity = "surat"
+console.log(accountId);
+console.log(accountEmail);
+console.table([accountPassword,accountCity])
+
+//let hi mostly use hota he..
+/*
+prefer not to use var 
+because of issue in block scope and function scope
+*/
