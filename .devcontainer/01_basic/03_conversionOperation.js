fcc85373e6942844273020a67040b2ff690278fd
Tanvi sonani <109598478+tanvipatel123@users.noreply.github.com>
2024-02-04 15:59:39 +0000

    ConversionOfDataTypes

---
+++ .devcontainer/01_basic/03_conversionOperation.js
@@ -0,0 +1,29 @@
+let score = undefined
+
+console.log(typeof score);
+console.log(typeof (score));
+
+let valueInNumber = Number (score);
+console.log(typeof valueInNumber);
+console.log(valueInNumber);//output: nan (not a number)
+
+//null = 0 ata he
+//33abc = nan ata hai , string number me convert na ho tab nan ata hai.
+//true = 1 ata hai 
+//false = 0 ata hai 
+//undefined = nan
+//33 = 33
+
+let isLoggedIn = ""
+let booleanIsLoggedIn = Boolean(isLoggedIn)
+console.log(booleanIsLoggedIn);
+
+//1 = true
+//"" = false
+//"tanvi" = true
+//0 = false
+
+let number = 33
+let stringNumber =  String(number)
+console.log( typeof stringNumber);
+//33 types of conver string me ans will be = string
