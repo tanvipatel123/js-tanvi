1a1e7ca1891c4da49cc14ec9b0c7902996d77be4
Tanvi sonani <109598478+tanvipatel123@users.noreply.github.com>
2024-02-04 15:42:55 +0000

    datatypes

---
+++ .devcontainer/01_basic/02_datatypes.js
@@ -0,0 +1,25 @@
+"use strict"; // treat all js code as newer version
+//data types..
+
+//alert (3+3) // we are using node js not browsers..
+
+console.log(3 +
+    3)
+    //code redability should be high
+
+let name ="Tanvi" //string
+let age = 21 //number
+let isLoggedIn = false //boolean
+
+//number +> 2 to power 53
+//bigint
+//string =>""
+//boolean => true , false
+//null => standalone  value, empty value usko assign kar sakte he (null).
+//undefined --variable define kiya hai but value nahi di hai.
+//symbol =>uniqueness find karne ke liye 
+
+//object
+console.log(typeof "Tanvi");//string
+console.log(typeof null);//object hai 
+console.log(typeof undefined);//undefined
